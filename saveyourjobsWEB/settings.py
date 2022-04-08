3fe2a70aafa3be1b5b57c49d6354c76f62901421
bigmurtaza <muratcansarkalkan@gmail.com>
2022-04-09 00:25:11 +0300

    Fixed something

--- saveyourjobsWEB/settings.py
+++ saveyourjobsWEB/settings.py
@@ -11,22 +11,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z+vfkx)!2hr^w=mlks@=b@37#!z($)vp83a44l2x2!rjoki_$d'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
